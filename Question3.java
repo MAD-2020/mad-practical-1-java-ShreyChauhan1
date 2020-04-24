527dd70c11957b3f15d9f0fc4e7d32c962ee8d46
ShreyChauhan1 <64241984+ShreyChauhan1@users.noreply.github.com>
2020-04-24 19:14:31 +0800

    Complete

--- Question3.java
+++ Question3.java
@@ -11,6 +11,9 @@ public class Question3
      */
      
     Scanner in = new Scanner(System.in);
+    int number = in.nextInt();
+    int product = number * number;
+    System.out.printIn(product);
     
   }
 }
