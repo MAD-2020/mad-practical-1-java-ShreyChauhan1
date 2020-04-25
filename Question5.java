f24fc960cc745f51f71701aefffe4af10f9ce1fd
ShreyChauhan1 <64241984+ShreyChauhan1@users.noreply.github.com>
2020-04-25 12:29:27 +0800

    Complete

--- Question5.java
+++ Question5.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Question5
 {
@@ -26,7 +27,32 @@ public class Question5
      * Hint: Use a loop to get input. Use another 2 loops to find the mode
      */
      
-    Scanner in = new Scanner(System.in);
-    
+       Scanner in = new Scanner(System.in);
+ 
+       ArrayList<Integer> data = new ArrayList<>();
+
+
+       System.out.print("Enter the number of digits you will enter: ");
+       int digits = in.nextInt();
+       for( int i=0; i<digits; i++)
+       {
+         System.out.print("Enter a digit: ");
+         int number = in.nextInt();
+         data.add(number);
+       }
+       int maxcount=0;
+       int maxvalue=0;
+       for (int j = 0; j < data.size(); j++) {
+         int count = 0;
+         for (int k = 0; k < data.size(); k++) {
+            if (data.get(k) == data.get(j)) count++;
+            }
+            if (count > maxcount) {
+              maxcount = count;
+              maxvalue = data.get(j);
+               }
+        }
+        System.out.println("Mode is "+ maxvalue);
+
   }
 }
